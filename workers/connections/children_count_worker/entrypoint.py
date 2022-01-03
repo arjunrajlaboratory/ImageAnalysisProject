718c62cc0bf8899f85c593634b15d3a4cce0a963
William Niu <wniu721@gmail.com>
2022-01-02 19:59:55 -0500

    Add children count worker for connections.

---
+++ workers/connections/children_count_worker/entrypoint.py
@@ -0,0 +1,81 @@
+import argparse
+import json
+import sys
+
+import annotation_client.annotations as annotations
+
+import networkx as nx
+import numpy as np
+
+
+def main(datasetId, apiUrl, token, params):
+    """
+    Params is a dict containing the following parameters:
+    required:
+        name: The name of the property
+        id: The id of the property
+        propertyType: can be "morphology", "relational", or "layer"
+    optional:
+        annotationId: A list of annotation ids for which the property should be computed
+        shape: The shape of annotations that should be used
+        layer: Which specific layer should be used for intensity calculations
+        tags: A list of annotation tags, used when counting for instance the number of connections to specific tagged annotations
+    """
+    propertyName = params.get('customName', None)
+    if not propertyName:
+        propertyName = params.get('name', 'unknown_property')
+
+    connectionIds = params.get('connectionIds', None)
+
+    # Setup helper classes with url and credentials
+    annotationClient = annotations.UPennContrastAnnotationClient(
+        apiUrl=apiUrl, token=token)
+
+    connectionList = []
+    if connectionIds:
+        # Get the annotations specified by id in the parameters
+        for id in connectionIds:
+            connectionList.append(annotationClient.getAnnotationConnectionById(id))
+    else:
+        # Get all point annotations from the dataset
+        connectionList = annotationClient.getAnnotationConnections(datasetId)
+
+    # We need at least one annotation
+    if len(connectionList) == 0:
+        return
+
+    edges = np.array([[connection['parentId'], connection['childId']] for connection in connectionList])
+    nodes = np.unique(edges)
+    node_attributes = [(node, annotationClient.getAnnotationById(node)) for node in nodes]
+
+    graph = nx.DiGraph()
+
+    graph.add_nodes_from(node_attributes)
+    graph.add_edges_from(edges)
+
+    graph = nx.DiGraph()
+
+    graph.add_nodes_from(node_attributes)
+    graph.add_edges_from(edges)
+
+    for node in nodes:
+
+        children = list(graph.successors(node))
+        annotationClient.addAnnotationPropertyValues(datasetId, node, {
+            propertyName: len(children)})
+
+
+if __name__ == '__main__':
+    # Define the command-line interface for the entry point
+    parser = argparse.ArgumentParser(
+        description='Compute average intensity values in a circle around point annotations')
+
+    parser.add_argument('--datasetId', type=str, required=True, action='store')
+    parser.add_argument('--apiUrl', type=str, required=True, action='store')
+    parser.add_argument('--token', type=str, required=True, action='store')
+    parser.add_argument('--parameters', type=str,
+                        required=True, action='store')
+
+    args = parser.parse_args(sys.argv[1:])
+
+    main(args.datasetId, args.apiUrl, args.token, json.loads(args.parameters))
