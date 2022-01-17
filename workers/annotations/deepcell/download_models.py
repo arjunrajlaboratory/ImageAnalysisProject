7878e0ef22fd99a6dc14a778a5bf0dba78b0d63e
William Niu <wniu721@gmail.com>
2022-01-17 00:58:56 -0500

    Add deepcell worker for annotations.

---
+++ workers/annotations/deepcell/download_models.py
@@ -0,0 +1,3 @@
+from deepcell.applications import Mesmer
+
+Mesmer()
---
+++ workers/annotations/deepcell/entrypoint.py
@@ -0,0 +1,97 @@
+import argparse
+import json
+import sys
+
+from operator import itemgetter
+
+import annotation_client.annotations as annotations
+import annotation_client.tiles as tiles
+
+import imageio
+
+import numpy as np  # library for array manipulation
+from deepcell.applications import Mesmer
+from rasterio.features import shapes
+
+
+def main(datasetId, apiUrl, token, params):
+    """
+    params (could change):
+        configurationId,
+        datasetId,
+        description: tool description,
+        type: tool type,
+        id: tool id,
+        name: tool name,
+        image: docker image,
+        channel: annotation channel,
+        assignment: annotation assignment ({XY, Z, Time}),
+        tags: annotation tags (list of strings),
+        tile: tile position (TODO: roi) ({XY, Z, Time}),
+        connectTo: how new annotations should be connected
+    """
+    # roughly validate params
+    keys = ["assignment", "channel", "connectTo", "tags", "tile"]
+    if not all(key in params for key in keys):
+        print("Invalid worker parameters", params)
+        return
+    assignment, channel, connectTo, tags, tile = itemgetter(*keys)(params)
+
+    # Setup helper classes with url and credentials
+    annotationClient = annotations.UPennContrastAnnotationClient(
+        apiUrl=apiUrl, token=token)
+    datasetClient = tiles.UPennContrastDataset(
+        apiUrl=apiUrl, token=token, datasetId=datasetId)
+
+    # TODO: will need to iterate or stitch and handle roi and proper intensities
+    pngBuffer = datasetClient.getRawImage(tile['XY'], tile['Z'], tile['Time'], channel)
+    stack = imageio.imread(pngBuffer)
+
+    # Expand image dimensions to rank 4
+    stack = np.stack((stack, stack), axis=-1)
+    stack = np.expand_dims(stack, axis=0)
+
+    # Create the application
+    app = Mesmer()
+
+    # create the lab
+    masks = app.predict(stack)[0, :, :, 0]
+
+    polygons = shapes(masks.astype(np.int32), masks > 0)
+
+    # Upload annotations TODO: handle connectTo. could be done server-side via special api flag ?
+    print(f"Uploading {masks.max()} annotations")
+    count = 0
+    for polygon, _ in polygons:
+        annotation = {
+            "tags": tags,
+            "shape": "polygon",
+            "channel": channel,
+            "location": {
+                "XY": assignment['XY'],
+                "Z": assignment['Z'],
+                "Time": assignment['Time']
+            },
+            "datasetId": datasetId,
+            "coordinates": [{"x": float(x), "y": float(y), "z": 0} for x, y in polygon['coordinates'][0]]
+        }
+        annotationClient.createAnnotation(annotation)
+        if count > 1000:  # TODO: arbitrary limit to avoid flooding the server if threshold is too big
+            break
+        count = count + 1
+
+
+if __name__ == '__main__':
+    # Define the command-line interface for the entry point
+    parser = argparse.ArgumentParser(
+        description='Compute average intensity values in a circle around point annotations')
+
+    parser.add_argument('--datasetId', type=str, required=True, action='store')
+    parser.add_argument('--apiUrl', type=str, required=True, action='store')
+    parser.add_argument('--token', type=str, required=True, action='store')
+    parser.add_argument('--parameters', type=str,
+                        required=True, action='store')
+
+    args = parser.parse_args(sys.argv[1:])
+
+    main(args.datasetId, args.apiUrl, args.token, json.loads(args.parameters))
