068666e2cdc8de90aa13be985b256b51b7c2a239
William Niu <wniu721@gmail.com>
2022-03-30 02:08:08 -0400

    Support generating preview and interface data for stack_spot_finder.

--- workers/annotations/stack_spot_finder/entrypoint.py
+++ workers/annotations/stack_spot_finder/entrypoint.py
@@ -1,3 +1,4 @@
+import base64
 import argparse
 import json
 import sys
@@ -6,12 +7,77 @@ from operator import itemgetter
 
 import annotation_client.annotations as annotations
 import annotation_client.tiles as tiles
+import annotation_client.workers as workers
 
 import imageio
 import numpy as np
 
 from skimage import filters
 from skimage.feature import peak_local_max
+
+
+def preview(datasetId, apiUrl, token, params, image):
+    # Setup helper classes with url and credentials
+    client = workers.UPennContrastWorkersClient(apiUrl=apiUrl, token=token)
+    datasetClient = tiles.UPennContrastDataset(
+        apiUrl=apiUrl, token=token, datasetId=datasetId)
+
+    keys = ["assignment", "channel", "connectTo", "tags", "tile", "workerInterface"]
+    assignment, channel, connectTo, tags, tile, workerInterface = itemgetter(*keys)(params)
+    thresholdValue = workerInterface['Threshold']['value']
+    sigma = workerInterface['Gaussian Sigma']['value']
+
+    # Get the tile
+    pngBuffer = datasetClient.getRawImage(tile['XY'], tile['Z'], tile['Time'], channel)
+    pngImage = imageio.imread(pngBuffer)
+
+    (width, height) = np.shape(pngImage)
+
+    gaussian = filters.gaussian(pngImage, sigma=sigma, mode='nearest')
+    laplacian = filters.laplace(gaussian)
+    laplacian = laplacian / np.max(laplacian)
+
+    # Compute the threshold indexes
+    index = laplacian > thresholdValue
+
+    # Convert image to RGB
+    rgba = np.zeros((width, height, 4), np.uint8)
+
+    # Paint threshold areas red
+    rgba[index] = [255, 0, 0, 255]
+
+    # Generate an output data-uri from the threshold image
+    outputPng = imageio.imwrite('<bytes>', rgba, format='png')
+    data64 = base64.b64encode(outputPng)
+    dataUri = 'data:image/png;base64,' + data64.decode('ascii')
+
+    # Send the preview object to the server
+    preview = {
+        'image': dataUri
+    }
+    client.setWorkerImagePreview(image, preview)
+
+
+def interface(image, apiUrl, token):
+    client = workers.UPennContrastWorkersClient(apiUrl=apiUrl, token=token)
+
+    # Available types: number, text, tags, layer
+    interface = {
+        'Gaussian Sigma': {
+            'type': 'number',
+            'min': 0,
+            'max': 5,
+            'default': 2
+        },
+        'Threshold': {
+            'type': 'number',
+            'min': 0,
+            'max': 1,
+            'default': 0.25
+        },
+    }
+    # Send the interface object to the server
+    client.setWorkerImageInterface(image, interface)
 
 
 def main(datasetId, apiUrl, token, params):
@@ -29,16 +95,27 @@ def main(datasetId, apiUrl, token, params):
         tags: annotation tags (list of strings),
         tile: tile position (TODO: roi) ({XY, Z, Time}),
         connectTo: how new annotations should be connected
+        request: 'compute', 'preview', 'interface'
+        interface: dictionary containing parameters associated to their ids
     """
-    # roughly validate params
-    keys = ["assignment", "channel", "connectTo", "tags", "tile"]
-    if not all(key in params for key in keys):
-        print("Invalid worker parameters", params)
-        return
-    assignment, channel, connectTo, tags, tile = itemgetter(*keys)(params)
 
-    # TODO: completely arbitrary
-    threshold = 0.001
+    # Check whether we need to preview, send the interface, or compute
+    request = params.get('request', 'compute')
+    if request == 'preview':
+        return preview(datasetId, apiUrl, token, params, params['image'])
+    if request == 'interface':
+        return interface(params['image'], apiUrl, token)
+
+    # roughly validate params
+    keys = ["assignment", "channel", "connectTo", "tags", "tile", "workerInterface"]
+    if not all(key in params for key in keys):
+        print ("Invalid worker parameters", params)
+        return
+    assignment, channel, connectTo, tags, tile, workerInterface = itemgetter(*keys)(params)
+
+    # Get the Gaussian sigma and threshold from interface values
+    threshold = workerInterface['Threshold']['value']
+    sigma = workerInterface['Gaussian Sigma']['value']
 
     # Setup helper classes with url and credentials
     annotationClient = annotations.UPennContrastAnnotationClient(
@@ -56,7 +133,7 @@ def main(datasetId, apiUrl, token, params):
     stack = np.stack(stack)
 
     # Filter
-    gaussian = filters.gaussian(stack, sigma=2, mode='nearest')
+    gaussian = filters.gaussian(stack, sigma=sigma, mode='nearest')
     laplacian = filters.laplace(gaussian)
 
     # Find local maxima
@@ -91,11 +168,12 @@ def main(datasetId, apiUrl, token, params):
 
 
 if __name__ == '__main__':
+
     # Define the command-line interface for the entry point
     parser = argparse.ArgumentParser(
         description='Compute average intensity values in a circle around point annotations')
 
-    parser.add_argument('--datasetId', type=str, required=True, action='store')
+    parser.add_argument('--datasetId', type=str, required=False, action='store')
     parser.add_argument('--apiUrl', type=str, required=True, action='store')
     parser.add_argument('--token', type=str, required=True, action='store')
     parser.add_argument('--parameters', type=str,
