bcf89db0743b0c4860f2dfc7f1a25f5a619a0c31
William Niu <wniu721@gmail.com>
2022-09-24 13:58:54 -0400

    Add blob_area_worker and blob_intensity_sum_worker.

---
+++ workers/properties/blobs/blob_intensity_sum_worker/entrypoint.py
@@ -0,0 +1,59 @@
+import argparse
+import json
+import sys
+
+import annotation_client.workers as workers
+
+import numpy as np
+from skimage import draw
+
+
+def main(datasetId, apiUrl, token, params):
+    """
+    Params is a dict containing the following parameters:
+    required:
+        name: The name of the property
+        id: The id of the property
+        propertyType: can be "morphology", "relational", or "layer"
+    optional:
+        annotationId: A list of annotation ids for which the property should be computed
+        shape: The shape of annotations that should be used
+        layer: Which specific layer should be used for intensity calculations
+        tags: A list of annotation tags, used when counting for instance the number of connections to specific tagged annotations
+    """
+
+    workerClient = workers.UPennContrastWorkerClient(datasetId, apiUrl, token, params)
+    annotationList = workerClient.get_annotation_list_by_shape('polygon')
+
+    # We need at least one annotation
+    if len(annotationList) == 0:
+        return
+
+    for annotation in annotationList:
+
+        image = workerClient.get_image_for_annotation(annotation)
+
+        if image is None:
+            continue
+
+        polygon = np.array([list(coordinate.values())[1::-1] for coordinate in annotation['coordinates']])
+        mask = draw.polygon2mask(image.shape, polygon)
+        intensity = np.sum(image[mask])
+
+        workerClient.add_annotation_property_values(annotation, float(intensity))
+
+
+if __name__ == '__main__':
+    # Define the command-line interface for the entry point
+    parser = argparse.ArgumentParser(
+        description='Compute average intensity values in a circle around point annotations')
+
+    parser.add_argument('--datasetId', type=str, required=False, action='store')
+    parser.add_argument('--apiUrl', type=str, required=True, action='store')
+    parser.add_argument('--token', type=str, required=True, action='store')
+    parser.add_argument('--parameters', type=str,
+                        required=True, action='store')
+
+    args = parser.parse_args(sys.argv[1:])
+
+    main(args.datasetId, args.apiUrl, args.token, json.loads(args.parameters))
