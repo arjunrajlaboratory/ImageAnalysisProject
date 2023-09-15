86a2c1f723f6a45d05eedea82a104d77e9916587
arjunrajlab <arjunrajlab@gmail.com>
2023-09-15 12:59:56 -0400

    Works across both architectures.

--- build_property_and_annotation_workers2.sh
+++ build_property_and_annotation_workers2.sh
@@ -3,10 +3,14 @@
 # Detect architecture
 ARCH=$(uname -m)
 
+echo "Architecture: $ARCH"
+
 # Set Dockerfile based on architecture
 if [ "$ARCH" == "arm64" ]; then
+    ECHO "Compiling for M1 architecture"
     DOCKERFILE="Dockerfile_M1"
 else
+    ECHO "Compiling for Intel architecture"
     DOCKERFILE="Dockerfile"
 fi
 
