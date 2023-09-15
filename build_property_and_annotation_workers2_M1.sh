08497665a495207baf7f7c0f76a3beaf0cdd31a7
arjunrajlab <arjunrajlab@gmail.com>
2023-09-15 12:38:48 -0400

    Two Dockerfiles

--- build_property_and_annotation_workers2_M1.sh
+++ build_property_and_annotation_workers2_M1.sh
@@ -10,3 +10,10 @@ docker build ./workers/properties/lines/line_length_worker/ -t properties/line_l
 docker build ./workers/properties/points/point_to_nearest_point_distance/ -t properties/point_to_nearest_point_distance:latest --build-arg BASE_IMAGE=ghcr.io/arjunrajlaboratory/base_m1_image:latest
 
 docker build ./workers/properties/points/point_to_nearest_connected_point_distance/ -t properties/point_to_nearest_connected_point_distance:latest --build-arg BASE_IMAGE=ghcr.io/arjunrajlaboratory/base_m1_image:latest
+############################################################################################################################################################################
+
+# M1
+docker build -f ./workers/annotations/connect_to_nearest/Dockerfile_M1 -t annotations/connect_to_nearest:latest ./workers/annotations/connect_to_nearest/
+
+# INTEL
+docker build -f ./workers/annotations/connect_to_nearest/Dockerfile -t annotations/connect_to_nearest:latest ./workers/annotations/connect_to_nearest/
--- build_property_and_annotation_workers2_intel.sh
+++ build_property_and_annotation_workers2_intel.sh
@@ -1,2 +1,5 @@
 docker build ./workers/annotations/random_square/ -t annotations/random_square:latest --build-arg BASE_IMAGE=ghcr.io/arjunrajlaboratory/base_x86_image:latest
 docker build ./workers/properties/lines/line_length_worker/ -t annotations/line_length:latest --build-arg BASE_IMAGE=ghcr.io/arjunrajlaboratory/base_x86_image:latest
+
+# INTEL
+docker build -f ./workers/annotations/connect_to_nearest/Dockerfile -t annotations/connect_to_nearest:latest ./workers/annotations/connect_to_nearest/
