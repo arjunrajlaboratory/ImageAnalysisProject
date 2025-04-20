550694937d7c2e580f61d89ca74eb78cbe21c267
Arjun Raj <arjunrajlab@gmail.com>
2025-04-20 15:02:54 -0400

    Base docker image built, untested

--- workers/base_docker_images/test_image/script.sh
+++
@@ -1,9 +0,0 @@
-# This script builds the base docker image (test)
-# Run the following commented out command with your personal access token.
-#export CR_PAT=[your personal access token]
-
-echo $CR_PAT | docker login ghcr.io -u arjunrajlab --password-stdin
-
-docker build -f Dockerfile -t ghcr.io/arjunrajlaboratory/test_image:latest .
-docker push ghcr.io/arjunrajlaboratory/test_image:latest
-
